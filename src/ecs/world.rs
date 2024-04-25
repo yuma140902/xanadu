ab74f619353ac0c7a7066ef6bdab0bf74b76dc04
yuma14 <yuma140902@gmail.com>
2024-04-26 00:29:22 +0900

    feat: add WorldBuilder::register_component

--- src/ecs/world.rs
+++ src/ecs/world.rs
@@ -1,4 +1,8 @@
+use std::{any::TypeId, collections::HashMap};
+
 use crate::generational_vec::{GenerationalId, GenerationalVec};
+
+use super::dyn_pool::{AnyComponentArray, ComponentArray};
 
 pub struct WorldBuilder {
     world: World,
@@ -10,6 +14,12 @@ impl WorldBuilder {
             world: World::new(),
         }
     }
+
+    pub fn register_component<T: bytemuck::Pod>(&mut self) -> &mut Self {
+        self.world.register_component::<T>();
+        self
+    }
+
     pub fn build(self) -> World {
         self.world
     }
@@ -23,13 +33,20 @@ impl Default for WorldBuilder {
 
 pub struct World {
     entities: GenerationalVec<()>,
+    component_arrays: HashMap<TypeId, AnyComponentArray>,
 }
 
 impl World {
     pub(self) fn new() -> Self {
         Self {
             entities: GenerationalVec::new(),
+            component_arrays: HashMap::new(),
         }
+    }
+
+    pub(self) fn register_component<T: bytemuck::Pod>(&mut self) {
+        self.component_arrays
+            .insert(TypeId::of::<T>(), ComponentArray::<T>::new().into());
     }
 
     pub fn new_entity(&mut self) -> GenerationalId {
@@ -45,15 +62,18 @@ impl Default for World {
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
     fn new() {
-        let world = super::World::new();
-        assert_eq!(world.entities.len(), 0);
+        let world = World::new();
+        assert!(world.entities.is_empty());
+        assert!(world.component_arrays.is_empty());
     }
 
     #[test]
     fn new_entity() {
-        let mut world = super::World::new();
+        let mut world = World::new();
         let entity = world.new_entity();
         assert_eq!(world.entities.len(), 1);
         assert_eq!(world.entities.get(entity), Some(&()));
